439f9fdd71e41c0b8320ab8b4cdebd3b0d03d69a
chinhcomhut <chinhcomhut@gmail.com>
2020-09-03 15:44:14 +0700

    update

--- src/main/java/com/codegym/wbdlaptop/model/User.java
+++ src/main/java/com/codegym/wbdlaptop/model/User.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Set;
 
 @Entity
-@Table(name = "user" , uniqueConstraints = {
+@Table(name = "user", uniqueConstraints = {
         @UniqueConstraint(columnNames = {
                 "username"
         }),
@@ -25,107 +25,47 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @NotBlank
     @Size(min = 3, max = 50)
     private String name;
-
     @NotBlank
-    @Size(min = 3 , max = 50)
+    @Size(min = 3, max = 50)
     private String username;
-
-    @NaturalId
     @NotBlank
     @Size(max = 50)
     @Email
     private String email;
-
     @JsonIgnore
     @NotBlank
-    @Size(min = 6 ,max = 100)
+    @Size(min = 6, max = 100)
     private String password;
 
     @JsonIgnore
     @ManyToMany(fetch = FetchType.LAZY)
-    @JoinTable(name = "user_roles" ,
+    @JoinTable(name = "user_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
-
-    @JsonIgnore
-    @OneToMany(targetEntity = Product.class, mappedBy = "user", cascade = CascadeType.ALL)
-    private List<Product> products;
-
-    @JsonIgnore
-    @OneToMany(targetEntity = Comment.class, mappedBy = "user", cascade = CascadeType.ALL)
-    private List<Comment> comments;
-
     @Lob
     private String blobString;
-
     @Lob
     private String avatar;
 
-    public User(Long id, Set<Role> roles, List<Product> products, List<Comment> comments, String blobString, String avatar) {
+    public User(@NotBlank @Size(min = 3, max = 50) String name, @NotBlank @Size(min = 3, max = 50) String username,  String email, @NotBlank @Size(min = 6, max = 100) String password) {
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+    public User(Long id, @NotBlank @Size(min = 3, max = 50) String name, @NotBlank @Size(min = 3, max = 50) String username, @NotBlank @Size(max = 50) @Email String email, @NotBlank @Size(min = 6, max = 100) String password, Set<Role> roles, String blobString, String avatar) {
         this.id = id;
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
         this.roles = roles;
-        this.products = products;
-        this.comments = comments;
         this.blobString = blobString;
         this.avatar = avatar;
-    }
-
-    public String getBlobString() {
-        return blobString;
-    }
-
-    public void setBlobString(String blobString) {
-        this.blobString = blobString;
-    }
-
-    public String getAvatar() {
-        return avatar;
-    }
-
-    public void setAvatar(String avatar) {
-        this.avatar = avatar;
-    }
-
-    public User() {
-    }
-
-    public User(@NotBlank @Size(min = 3, max = 50) String name, @NotBlank @Size(min = 3, max = 50) String username, @NotBlank @Size(max = 50) @Email String email, @NotBlank @Size(min = 6, max = 100) String password, Set<Role> roles, List<Product> products, List<Comment> comments) {
-        this.name = name;
-        this.username = username;
-        this.email = email;
-        this.password = password;
-        this.roles = roles;
-        this.products = products;
-        this.comments = comments;
-    }
-
-    public User(@NotBlank @Size(min = 3, max = 50) String name, @NotBlank @Size(min = 3, max = 50) String username, @NotBlank @Size(max = 50) @Email String email, @NotBlank @Size(min = 6, max = 100) String password, Set<Role> roles) {
-        this.name = name;
-        this.username = username;
-        this.email = email;
-        this.password = password;
-        this.roles = roles;
-    }
-
-    public User(@NotBlank @Size(min = 3, max = 50) String name, @NotBlank @Size(min = 3, max = 50) String username, @NotBlank @Size(max = 50) @Email String email, @NotBlank @Size(min = 6, max = 100) String password, Set<Role> roles, List<Product> products) {
-        this.name = name;
-        this.username = username;
-        this.email = email;
-        this.password = password;
-        this.roles = roles;
-        this.products = products;
-    }
-
-    public User(@NotBlank @Size(min = 3, max = 50) String name, @NotBlank @Size(min = 3, max = 50) String username, @NotBlank @Size(max = 50) @Email String email, @NotBlank @Size(min = 6, max = 100) String password) {
-        this.name = name;
-        this.username = username;
-        this.email = email;
-        this.password = password;
     }
 
     public Long getId() {
@@ -176,19 +116,19 @@ public class User {
         this.roles = roles;
     }
 
-    public List<Product> getProducts() {
-        return products;
+    public String getBlobString() {
+        return blobString;
     }
 
-    public void setProducts(List<Product> products) {
-        this.products = products;
+    public void setBlobString(String blobString) {
+        this.blobString = blobString;
     }
 
-    public List<Comment> getComments() {
-        return comments;
+    public String getAvatar() {
+        return avatar;
     }
 
-    public void setComments(List<Comment> comments) {
-        this.comments = comments;
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
     }
 }
