c24512213e76af937ea02ffa2d88655a17c6a843
chinhcomhut <chinhcomhut@gmail.com>
2020-09-12 16:43:43 +0700

    update

--- src/main/java/com/codegym/wbdlaptop/controller/SongAPI.java
+++ src/main/java/com/codegym/wbdlaptop/controller/SongAPI.java
@@ -85,7 +85,7 @@ public class SongAPI {
             return new ResponseEntity<>(new ResponseMessage("nocategory"), HttpStatus.OK);
         }
 
-        if (song.getNameSinger() == "" && song.getNameBand()=="") {
+        if ((song.getNameSinger() == ""||song.getNameSinger()==null) && (song.getNameBand()==""||song.getNameBand()==null)) {
             return new ResponseEntity<>(new ResponseMessage("nosingerband"), HttpStatus.OK);
         }
 
